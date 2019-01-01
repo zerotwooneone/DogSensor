745070f6051477ee4f3b00a3959b0ff6b13ca04f
David Squire <squiredd@gmail.com>
2019-01-01 14:27:33 -0800

    changed to use SoftTimer library

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # DogSensor
+
+Depends upon these libraries (you must install them from Arduio IDE, for example)
+
+* SoftTimer
+* PciManager
--- ultraSonic/ultraSonic.ino
+++ ultraSonic/ultraSonic.ino
@@ -1,77 +1,66 @@
-/*
- * created by Rui Santos, https://randomnerdtutorials.com
- * 
- * Complete Guide for Ultrasonic Sensor HC-SR04
- *
-    Ultrasonic sensor Pins:
-        VCC: +5VDC
-        Trig : Trigger (INPUT) - Pin11
-        Echo: Echo (OUTPUT) - Pin 12
-        GND: GND
- */
- 
-int trigPin = 11;    // Trigger
-int echoPin = 12;    // Echo
+#include <SoftTimer.h>
+#include <DelayRun.h>
 
-
+const int LedPin = 13;
 const int buzzerPin = 3;//the buzzer pin attach to
-int fre;//set the variable to store the frequence value
+const int trigPin = 11;    // Trigger
+const int echoPin = 12;    // Echo
 
-const int minimumCentimeters =91;
- 
+// -- Define method signatures.
+boolean turnOff(Task* task);
+boolean turnOn(Task* task);
+boolean turnOnTrig(Task* task);
+boolean turnOffTrig(Task* task);
+
+const int trigCleanupMilliseconds = 200;
+const int trigTaskMilliseconds = 10;
+DelayRun trigOffTask(trigTaskMilliseconds, turnOffTrig);
+DelayRun trigOnTask(trigCleanupMilliseconds, turnOnTrig, &trigOffTask);
+
 void setup() {
   //Serial Port begin
   Serial.begin (9600);
-  //Define inputs and outputs
+  
+  // -- We close the loop, so after offTask the onTask will start.
+  trigOffTask.followedBy = &trigOnTask;
+
+  pinMode(LedPin, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(buzzerPin,OUTPUT);
-}
- 
-void loop() {
-  long cm= getCentimeters();
-  
-  if(cm < minimumCentimeters)
-  {
-    for(int i = 200;i <= 800;i++) //frequence loop from 200 to 800
-    {
-      if(cm < minimumCentimeters){
-        tone(buzzerPin,i); //turn the buzzer on    
-      }else{
-        noTone(buzzerPin);      
-        break;
-      }
-      cm= getCentimeters();
-    }
-  }
-   noTone(buzzerPin);
+
+  // -- Start the offTask to take effect after 1 second.
+  trigOnTask.startDelayed();
 }
 
-/*
- * This takes at least 15 ms to run 
- */
-long getCentimeters(){
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
+boolean turnOnTrig(Task* task) {
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
+  return true; // -- Return true to enable the "followedBy" task.
+}
+boolean turnOffTrig(Task* task) {
   digitalWrite(trigPin, LOW);
- 
+
   // Read the signal from the sensor: a HIGH pulse whose
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
   long duration;
   duration = pulseIn(echoPin, HIGH);
  
   // Convert the time into a distance
   long cm;
   cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  
-  Serial.print(cm);
+
+  if(cm < 91){
+    digitalWrite(LedPin, HIGH);
+    tone(buzzerPin,cm); //turn the buzzer on
+  }else{
+    digitalWrite(LedPin, LOW);
+    noTone(buzzerPin);
+  }
+
+  /*Serial.print(cm);
   Serial.print("cm");
-  Serial.println();
-  return cm;
+  Serial.println();*/
+  
+  return true; // -- Return true to enable the "followedBy" task.
 }
